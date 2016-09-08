e75a6fb4d1c64437169e8f7747f0df989f16c227
Walter Stanish <walter@stani.sh>
2016-09-09 03:02:57 +0800

    Area instantiation

--- areas.lua
+++ areas.lua
@@ -3,6 +3,20 @@ areas={}
 -- area types
 area_types={}
 
+-- instantiate area
+function area_generate(z,x,y)
+	local new_area = {}
+	new_area.type=world[z][x][y].type
+	if world[z][x][y].name ~= nil then
+		new_area.name=world[z][x][y].name
+	end
+	setmetatable(new_area,{__index = area_types[area_type]})
+	if new_area.setup ~= nil then
+		new_area:setup(new_area)
+	end
+	world[z][x][y] = new_area
+end
+
 -- load areas
 require "areas/tai_cave"
 require "areas/tai_cave_entrance"
