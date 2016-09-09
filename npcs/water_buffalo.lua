f3bd7a5a7b8764c5f68be69da4d6841a0c9f8ccc
Walter Stanish <walter@stani.sh>
2016-09-09 16:56:19 +0800

    Add water buffalo NPC

---
+++ npcs/water_buffalo.lua
@@ -0,0 +1,25 @@
+npc_types['water_buffalo'] = {
+			name="Water Buffalo",
+			color={108,128,220},
+			hostile=false,
+			move='random',
+			sounds={
+				attack=love.audio.newSource({
+					"npcs/water_buffalo/buffalo-1.mp3",
+					"npcs/water_buffalo/buffalo-10.mp3",
+					"npcs/water_buffalo/buffalo-11.mp3",
+					"npcs/water_buffalo/buffalo-12.mp3",
+					"npcs/water_buffalo/buffalo-13.mp3",
+					"npcs/water_buffalo/buffalo-14.mp3",
+					"npcs/water_buffalo/buffalo-15.mp3",
+					"npcs/water_buffalo/buffalo-2.mp3",
+					"npcs/water_buffalo/buffalo-3.mp3",
+					"npcs/water_buffalo/buffalo-4.mp3",
+					"npcs/water_buffalo/buffalo-5.mp3",
+					"npcs/water_buffalo/buffalo-6.mp3",
+					"npcs/water_buffalo/buffalo-7.mp3",
+					"npcs/water_buffalo/buffalo-8.mp3",
+					"npcs/water_buffalo/buffalo-9.mp3"
+				})
+			}
+		}
