1ea5e69066c18b5a3a20f348767bda2e19eacee2
Walter Stanish <walter@stani.sh>
2016-09-12 07:29:32 +0800

    Add natural_cavern cave type

--- areas.lua
+++ areas.lua
@@ -14,6 +14,7 @@ function area_generate(z,x,y)
 	if world[z][x][y].name ~= nil then
 		new_area.name=world[z][x][y].name
 	end
+	print(" - Generating area type '" .. new_area.type .. "'.")
 	setmetatable(new_area,{__index = area_types[area_type]})
 	if area_types[new_area.type].setup ~= nil then
 		area_types[new_area.type].setup(new_area)
@@ -22,6 +23,7 @@ function area_generate(z,x,y)
 end
 
 -- load areas
+require "areas/natural_cavern"
 require "areas/tai_cave"
 require "areas/tai_cave_entrance"
 require "areas/tai_village"
---
+++ areas/natural_cavern.lua
@@ -0,0 +1,117 @@
+area_types['natural_cavern'] = {
+				 name  = 'Natural Cavern',
+				 setup = function(instance) 
+                                        instance.name='Cavern'
+                                        instance.prefix='Natural '
+
+					-- Generate an appropriate map
+					local new_tilemap
+					new_tilemap = {}
+                                        for i=1,resolutionTilesX,1 do
+                                                new_tilemap[i] = {}
+                                                for j=1,resolutionTilesY,1 do
+                                                        new_tilemap[i][j] = 0	-- initializes to rock
+                                                end
+                                        end
+
+					-- OLD generate tilemap
+					--[[
+					        cl=ROT.Map.Cellular:new(resolutionTilesX, resolutionTilesY) -- , {connected=true})
+					        cl:randomize(.50)  -- .50 is the probability any given tile is a floor
+					        cl=ROT.Map.Rogue:new(resolutionTilesX, resolutionTilesY)
+					        cl:create(tile_callback)
+					--]]
+					-- NEW generate tilemap
+        				--  width,
+        				--  height,
+        				--  changeDirectionModifier,    (10 = super straight, 90 = wiggly as hell)   = "wiggliness"
+        				--  sparsenessModifier,         (10 = full of corridors, 90 = full of rock)  = "rockiness"
+        				--  deadEndRemovalModifier,     (10 = lots of dead ends, 100 = no dead ends) = "connectedness"
+        				--  roomGenerator
+        				--    - noOfRoomsToPlace
+        				--    - minRoomWidth
+        				--    - maxRoomWidth
+        				--    - minRoomHeight
+        				--    - maxRoomHeight
+        				local symbols = {Wall=0, Empty=1, DoorN=2, DoorS=2, DoorE=2, DoorW=2}
+        				--local generator = astray.Astray:new( math.floor(resolutionTilesX/2), math.floor(resolutionTilesY/2), 30, 20, 90, astray.RoomGenerator:new(10,1,5,1,5) )
+        				--local generator = astray.Astray:new( resolutionTilesY-1, math.floor(resolutionTilesY/2)-1, 30, 20, 90, astray.RoomGenerator:new(10,1,5,1,5) )
+        				--local generator = astray.Astray:new( resolutionTilesX/2-1, resolutionTilesY/2-1, 30, 20, 90, astray.RoomGenerator:new(10,1,5,1,5) )
+				        local generator = astray.Astray:new( resolutionTilesX/2-1, resolutionTilesY/2-1, 80, 70, 100, astray.RoomGenerator:new(22,2,5,2,5) )
+				        local dungeon = generator:Generate()
+				        local tmp_tilemap = generator:CellToTiles(dungeon, symbols )
+  				      	-- the astray generator begins its tilemap at row 0 and column 0 instead of row 1 and column 1, which does not match other lua code
+ 				       	for y = 1, #tmp_tilemap[1] do
+						local line = ''
+                				for x = 1, #tmp_tilemap do
+                        				local nx=x-1
+                        				local ny=y-1
+                        				if tmp_tilemap[nx] ~= nil and tmp_tilemap[nx][ny] ~= nil then
+                                				new_tilemap[x][y] = tmp_tilemap[nx][ny]
+                        				end
+                				end
+        				end
+					-- assign new map
+					instance.map=new_tilemap
+
+					-- Populate with NPCs
+					instance.npcs = {}
+				        add_npcs(instance.npcs,'goblin',5)
+				        add_npcs(instance.npcs,'mouse',1)
+			
+				        -- place npcs
+--[[ ALL THESE RELY ON RANDOM LOCATIONS - THOSE FUNCTIONS ARE HARDCODED TO tilemap AND NEED REWRITING ...
+				        print "Randomly placing NPCs..."
+				        for i=1,#npcs,1 do
+				                npcs[i]['location'] = {}
+				                npcs[i]['location']['x'],npcs[i]['location']['y'] = randomStandingLocationWithoutNPCsOrPlayer()
+				        end
+				
+				        -- place ground features
+				        for i=1,120,1 do
+				                groundfeatures[i] = {}
+				                groundfeatures[i]['x'],groundfeatures[i]['y'] = randomStandingLocation()
+				                if i < 3 then
+				                        groundfeatures[i]['type'] = 'shrub'
+				                elseif i < 10 then
+				                        groundfeatures[i]['type'] = 'puddle'
+				                else
+				                        groundfeatures[i]['type'] = 'stone'
+				                end
+				        end
+				
+				        -- place stairs
+				        print "Randomly placing stairs..."
+				        stairsX, stairsY = randomStandingLocation()
+				        tilemap[stairsX][stairsY] = '>'
+				        stairsX, stairsY = randomStandingLocation()
+				        tilemap[stairsX][stairsY] = '<'
+--]]
+				
+				        -- music
+					--[[
+					instance.music = {
+				                                        "music/Greg_Reinfeld_-_02_-_Canon_in_D_ni_nonaC_Pachelbels_Canon.mp3",
+				                                        "music/Kevin MacLeod - Sardana.mp3",
+				                                        "music/Kevin MacLeod - Suonatore di Liuto.mp3",
+				                                        "music/Kevin MacLeod - Teller of the Tales.mp3",
+				                                        "music/Komiku_-_03_-_Champ_de_tournesol.mp3",
+				                                        "music/Komiku_-_05_-_La_Citadelle.mp3",
+				                                        "music/Komiku_-_06_-_La_ville_aux_ponts_suspendus.mp3"
+				                         }
+					instance.music_volume=0.05
+					--]]
+					-- ambient noise
+					instance.ambient = {
+				                                                "sounds/ambient/cave-drips.mp3"
+				                           }
+					instance.ambient_volume = 2
+
+					-- colors
+                                        instance.colors={}
+                                        instance.colors['groundColor'] = {25,25,25}
+
+					-- FOV
+					instance.fov = 15
+				 end
+		       	    }
