6bbbbe2409c605a6e944ca6a2131f3e84ac1bc0f
Walter Stanish <walter@stani.sh>
2016-09-13 19:15:19 +0800

    Many new functions

--- tilemap.lua
+++ tilemap.lua
@@ -28,12 +28,28 @@ directions   = {
 			{-1,1},
 			{1,-1},
 			{1,1}
-	       };
+	       }
+
+directions_by_name = {
+			left=1,
+			right=2,
+			up=3,
+			down=4,
+			upleft=5,
+			downleft=6,
+			upright=7,
+			downright=8
+		     }
+
+opposite_directions = { 2,1,4,3,8,7,6,5 }
+
 
 -- New tilemap
 function tilemap_new(width,height,defaultmaptiletype)
-	width = math.floor(width) or resolutionTilesX
-	height = math.floor(height) or resolutionTilesY
+	width = width or resolutionTilesX
+	width = math.floor(width)
+	height = height or resolutionTilesY
+	height = math.floor(height)
 	defaultmaptiletype = defaultmaptiletype or 0
 	local m={}
 	for x=1,width,1 do
@@ -64,15 +80,15 @@ function tilemap_draw_rectangle(tilemap,sx,sy,width,height,maptiletype)
 		print("ERROR: tilemap_draw_rectangle(): Passed a nil tilemap.")
 		--os.exit()
 	end
-	print("tilemap_draw_rectangle() passed tilemap of dimensions " .. #tilemap .. "x" .. #tilemap[1] .. ", sx/sy = " .. sx .. "/" .. sy .. ", w/h = " .. width .. "/" .. height .. ", and maptiletype = '" .. maptiletype .. "'")
+	--print("tilemap_draw_rectangle() passed tilemap of dimensions " .. #tilemap .. "x" .. #tilemap[1] .. ", sx/sy = " .. sx .. "/" .. sy .. ", w/h = " .. width .. "/" .. height .. ", and maptiletype = '" .. maptiletype .. "'")
 	sx = math.floor(sx)
 	sy = math.floor(sy)
 	width = math.floor(width)
 	height = math.floor(height)
 	for x=sx,sx+width,1 do
 		for y=sy,sy+width,1 do
-			if x>0 and y>0 and x<#tilemap and y<#tilemap[1] then
-				print("tilemap_draw_rectangle() attempting to draw '" .. maptiletype .. "' at position " .. x .. "/" .. y .. " (vs. w/h " .. width .. "/" .. height .. ")")
+			if x>0 and y>0 and x<#tilemap and tilemap[1] ~= nil and y<#tilemap[1] then
+				--print("tilemap_draw_rectangle() attempting to draw '" .. maptiletype .. "' at position " .. x .. "/" .. y .. " (vs. w/h " .. width .. "/" .. height .. ")")
 				tilemap[x][y] = maptiletype
 			end
 		end
@@ -113,8 +129,12 @@ end
 
 -- Check the validity of a set of coordinates given a specific tilemap
 function tilemap_coordinates_valid(tilemap,x,y)
-	if math.floor(x) == x and math.floor(y) == y and x > 0 and y > 0 and x < #tilemap and y < #tilemap[1] then
-		return true
+	if x == nil or y == nil then
+		return false
+	elseif math.floor(x) == x and math.floor(y) == y and x > 0 and y > 0 and x < #tilemap and y < #tilemap[1] then
+		if tilemap[x] ~= nil and tilemap[x][y] ~= nil then
+			return true
+		end
 	end
 	return false
 end
@@ -125,19 +145,36 @@ end
 --  Otherwise, return nil ("no direction").
 function tilemap_door_direction(tilemap,x,y)
 
+	-- Input validation
+	x = math.floor(x)
+	y = math.floor(y)
+	if not tilemap_coordinates_valid(tilemap,x,y) then
+		print("ERROR: tilemap_door_direction() passed invalid tilemap x/y coordinates: " .. x .. "/" .. y)
+		return nil
+	end
+	
 	-- If the square is something other than rock, it's already occupied, and we
 	-- return a nil value to represent no direction
-	if tilemap[x][y] != 0 then
+	if tilemap[x][y] ~= 0 then
 		return nil
 	end
 
 	-- for the plain four directions (left, right, up, down)...
 	for dir=1, 4, 1 do
+--[[
+tilemap_draw_rectangle() attempting to draw '1' at position 42/15 (vs. w/h 5/4)
+Error: tilemap.lua:145: attempt to perform arithmetic on a nil value
+stack traceback:
+    tilemap.lua:145: in function 'tilemap_door_direction'
+    luaBrogueArchitect.lua:473: in function 'mapgen_broguestyle_attach_rooms'
+    luaBrogueArchitect.lua:38: in function 'mapgen_broguestyle'
+--]]
+--	print("tilemap_door_direction(): dir is " .. dir .. " and #directions is " .. #directions)
 		-- calculate coordinate offsets for adjacent tiles
-		newx = x + directions[dir][0]
-		newy = y + directions[dir][1]
-		oppx = x - directions[dir][0]
-		oppy = y - directions[dir][1]
+		newx = x + directions[dir][1]
+		newy = y + directions[dir][2]
+		oppx = x - directions[dir][1]
+		oppy = y - directions[dir][2]
 		-- if both sets of coordinates lie within the map and the
 		-- opposite coordinate is also floor (ie. standable)...
 		if tilemap_coordinates_valid(oppx,oppy) and tilemap_coordinates_valid(newx,newy) and tilemap[oppx][oppy] == 1 then
@@ -147,4 +184,26 @@ function tilemap_door_direction(tilemap,x,y)
 	end
 
 	return nil
+end
+
+-- get the opposite of a given direction
+function tilemap_opposite_direction(numeric_direction)
+	if opposite_directions[numeric_direction] ~= nil then
+		return opposite_directions[numeric_direction]
+	else
+		if directions_by_name[numeric_direction] ~= nil then
+			return opposite_directions[directions_by_name[numeric_directions]]
+		end
+	end
+end
+
+-- output a textual representation of a tilemap
+function tilemap_show(tilemap,title)
+	title = title or ''
+	print("================================ " .. title .. " ============================================================")
+	for i=1,#tilemap,1 do
+		line = table.concat(tilemap[i])
+		print(line)
+	end
+	print("============================================================================================================")
 end
